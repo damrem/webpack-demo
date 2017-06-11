34c075d98b36a47b9fa84f384e3d9f0aaf7d52a6
damrem <damien.remars@gmail.com>
2017-06-11 23:50:23 +0200

    add input to theme list

--- app/themes/ThemeList.jsx
+++ app/themes/ThemeList.jsx
@@ -4,17 +4,37 @@ import {ThemeManager} from './ThemeManager';
 
 export default
 class ThemeList extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {searchValue: ''};
+
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	onSearchChange(event) {
+		this.setState({searchValue: event.target.value});
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log(nextProps, nextState);
+	    return true;  
+	}
+
 	render(){
-		console.log(this.props);
+		// console.log(this.props);
 		return (!!this.props.themes &&
-			<ul className="ThemeList noBullet">
-				
-				{ this.props.themes.map((theme, i) => 
-					<li key={i}>
-						<ThemeItem theme={theme}/>
-					</li>) 
-				}
-			</ul>
+			<div>
+				<input type="text" value={this.state.searchValue} onChange={this.onSearchChange}/>
+				<ul className="ThemeList noBullet">
+					
+					{ this.props.themes.map((theme, i) => 
+						<li key={i}>
+							<ThemeItem theme={theme}/>
+						</li>) 
+					}
+				</ul>
+			</div>
 		);
 	}
 }
